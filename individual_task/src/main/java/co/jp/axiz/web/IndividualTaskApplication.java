6fad112990580b62ea6932711b45674dcb1efbfb
AxiZ <AxiZ@WINDOWS-BH68S1V>
2018-06-15 14:01:09 +0900

    個人開発プロジェクト生成

---
+++ individual_task/src/main/java/co/jp/axiz/web/IndividualTaskApplication.java
@@ -0,0 +1,12 @@
+package co.jp.axiz.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndividualTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndividualTaskApplication.class, args);
+	}
+}
